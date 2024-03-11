033ebb5741a9a815adcbc1af44c2512f77127f58
andraalayubi <andraalayubi@gmail.com>
2024-03-11 18:16:50 +0700

    Finishing API mobile

--- web_project/index.js
+++ web_project/index.js
@@ -52,7 +52,7 @@ app.get('/', async (req, res) => {
 
     } catch (err) {
         console.error(err);
-        res.status(500).send("Internal Server Error");
+        res.status(500).send(err);
     }
 });
 
@@ -70,12 +70,65 @@ app.get('/api/perusahaan', async (req, res) => {
 
     } catch (err) {
         console.error(err);
-        res.status(500).send("Internal Server Error");
+        res.status(500).send(err);
     }
 });
 
-app.get('/api/users/:id', (req, res) => {
+app.get('/api/perusahaan/:id', async (req, res) => {
+    try {
+        const id_perusahaan = req.params.id;
+        const sql = `SELECT perusahaan.nama_perusahaan, perusahaan.kota, perusahaan.provinsi, posisi.nama_posisi, COUNT(magang.siswa_id) AS jumlah_siswa FROM perusahaan JOIN posisi ON perusahaan.id_perusahaan = posisi.perusahaan_id LEFT JOIN magang ON posisi.id_posisi = magang.posisi_id WHERE perusahaan.id_perusahaan = ${id_perusahaan};`
+        const data = await new Promise((resolve, reject) => {
+            db.query(sql, function (err, result) {
+                if (err) reject(err);
+                resolve(result);
+            });
+        });
 
+        res.send(data);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
+});
+
+app.get('/api/perusahaan/:id/:id_posisi', async (req, res) => {
+    try {
+        const id_posisi = req.params.id_posisi;
+        const sql = `SELECT siswa.id_siswa, siswa.nama_siswa, siswa.email FROM siswa JOIN magang ON siswa.id_siswa = magang.siswa_id JOIN posisi ON magang.posisi_id = posisi.id_posisi WHERE posisi.id_posisi = ${id_posisi};`
+        const data = await new Promise((resolve, reject) => {
+            db.query(sql, function (err, result) {
+                if (err) reject(err);
+                resolve(result);
+            });
+        });
+
+        res.send(data);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
+});
+
+app.get('/api/user/:id', async (req, res) => {
+    try {
+        const id_siswa = req.params.id;
+        const sql = `SELECT * FROM magang JOIN posisi ON magang.posisi_id = posisi.id_posisi JOIN perusahaan ON posisi.perusahaan_id = perusahaan.id_perusahaan JOIN siswa ON magang.siswa_id = siswa.id_siswa WHERE siswa.id_siswa = ${id_siswa};`
+        const data = await new Promise((resolve, reject) => {
+            db.query(sql, function (err, result) {
+                if (err) reject(err);
+                resolve(result);
+            });
+        });
+
+        res.send(data);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
 });
 
 app.listen(port, () => {
