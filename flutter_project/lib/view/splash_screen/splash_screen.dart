87ecf691c64d7e61dcae81fa84a845d4c96527c3
= <=>
2024-03-18 15:04:26 +0700

    splashscreen onboarding finished

--- flutter_project/lib/view/splash_screen/splash_screen.dart
+++ flutter_project/lib/view/splash_screen/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/view/onboarding/onboarding.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -7,55 +8,63 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 20), // Changed from MainAxisAlignment to margin
-                    child: Image.asset(
-                      'assets/logo/logo2.png',
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => OnBoarding()));
+        },
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 20),
+                      // Changed from MainAxisAlignment to margin
+                      child: Image.asset(
+                        'assets/logo/logo2.png',
+                      ),
                     ),
-                  ),
-                  const Text(
-                    'GoShip',
-                    textAlign: TextAlign.center,
+                    const Text(
+                      'GoShip',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    // spacing between logo and "Your story became my apprentice" text
+                    const Text(
+                      'Your story became my apprentice',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.orange,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: const Text(
+                    'Versi 1.1.1',
                     style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
                       color: Colors.black,
                     ),
                   ),
-                  const SizedBox(height: 20), // spacing between logo and "Your story became my apprentice" text
-                  const Text(
-                    'Your story became my apprentice',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.orange,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: const Text(
-                  'Versi 1.1.1',
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.black,
-                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
