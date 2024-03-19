90ae104e19c3683369efec443ff08e17e97db74a
andraalayubi <andraalayubi@gmail.com>
2024-03-20 05:32:25 +0700

    connecting react to backend done

--- web_project/goship/src/App.js
+++ web_project/goship/src/App.js
@@ -1,22 +1,16 @@
 // App.js
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './App.css'; 
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    axios.get('http://localhost:5000')
-      .then(res => setMessage(res.data))
-      .catch(err => console.error(err));
-  }, []);
-
   return (
-    <div className="App">
-      <h1>{message}</h1>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
