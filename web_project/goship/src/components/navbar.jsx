f22e71a1bfc90183091ad9b9053ce97982db61b2
zayrats <113332851+zayrats@users.noreply.github.com>
2024-05-14 18:22:09 +0700

    Update navbar.jsx
    responsif

--- web_project/goship/src/components/navbar.jsx
+++ web_project/goship/src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { logo1, menu, close } from "../assets";
+import { logo1 } from "../assets";
 import { navLinks } from "../constant";
 import ButtonSignIn from "./button_SignIn.jsx";
 import { Link } from "react-router-dom";
@@ -10,7 +10,6 @@ const Navbar = (user) => {
   const [iniNavbar, setIniNavbar] = useState(false);
   const loggedUser = user.user;
   console.log("🚀 ~ Navbar ~ loggedUser:", loggedUser)
-  const [toggle, setToggle] = useState(false);
 
   const scrollHeader = () => {
     if (window.scrollY >= 20) {
@@ -29,56 +28,24 @@ const Navbar = (user) => {
 
   return (
     <div className={`fixed z-10 w-full transition duration-300 ${iniNavbar ? 'bg-orange-gradient-navbar' : 'bg-transparent'}`}>
-      <nav className="w-full flex sm:px-16 justify-between items-center navbar">
+      <nav className="header px-16 w-full flex justify-between items-center navbar">
         <img src={logo1} alt="Goship" className="w-[150px] h-[40px]" />
 
         <ul className="list-none sm:flex hidden justify-end items-center flex-1">
-        {navLinks.map((nav, index) => (
-          <li
-            key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] ${
-              active === nav.title ? "text-orange" : "text-dimblack"
-            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
-            onClick={() => setActive(nav.title)}
-          >
-            <a href={`#${nav.id}`}>{nav.title}</a>
-          </li>
-        ))}
-        <div className="sm:px-10 sm:py-3">
-          {loggedUser !== null ? <Link to='/student-profile' state={{ user: loggedUser }}>{loggedUser}</Link> : <ButtonSignIn />} 
-        </div>
-      </ul>
-      <div className="sm:hidden flex flex-1 justify-end items-center px-16">
-        <img
-          src={toggle ? close : menu}
-          alt="menu"
-          className="w-[28px] h-[28px] object-contain"
-          onClick={() => setToggle(!toggle)}
-        />
-
-        <div
-          className={`${
-            !toggle ? "hidden" : "flex"
-          } p-6 bg-white text-orange absolute top-20 right-0 mx-4 min-w-[140px] rounded-xl sidebar`}
-        >
-          <ul className="list-none flex justify-end items-start flex-1 flex-col">
-            {navLinks.map((nav, index) => (
-              <li
-                key={nav.id}
-                className={`font-poppins font-medium cursor-pointer text-[16px] ${
-                  active === nav.title ? "text-orange" : "text-black"
-                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
-                onClick={() => setActive(nav.title)}
-              >
-                <a href={`#${nav.id}`}>{nav.title}</a>
-              </li>
-            ))}
-             <div className="sm:px-10 sm:py-4">
-             {loggedUser !== null ? <Link to='/student-profile' state={{ user: loggedUser }}>{loggedUser}</Link> : <ButtonSignIn />}  
-            </div>
-          </ul>
-        </div>
-      </div>
+          {navLinks.map((nav, index) => (
+            <li
+              key={nav.id}
+              className={`font-poppins font-normal cursor-pointer text-[16px] ${active === nav.title ? "text-orange" : "text-dimblack"
+                } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+              onClick={() => setActive(nav.title)}
+            >
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))}
+          <div className="sm:px-10 px-5 sm:py-3">
+            {loggedUser !== null ? <Link to='/student-profile' state={{ user: loggedUser }}>{loggedUser}</Link> : <ButtonSignIn />}
+          </div>
+        </ul>
       </nav>
     </div>
   );
